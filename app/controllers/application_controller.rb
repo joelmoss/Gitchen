8740b5ed118133a3b88bb838e6a0ac1114f1ff91
Joel Moss <joel@developwithstyle.com>
2012-03-26 00:36:13 +0100

    Created sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :current_user
+  helper_method :signed_in?
+  
+
+  private
+  
+    def current_user
+      return nil unless session[:user_id]
+      
+      begin
+        @current_user ||= User.find(session[:user_id])
+      rescue ActiveRecord::RecordNotFound
+        nil
+      end
+    end
+
+    def signed_in?
+      !current_user.nil?
+    end
+
+    def authenticate_user!
+      unless signed_in?
+        redirect_to sign_in_url, :alert => 'You need to sign in for access to this page.'
+      end
+    end
+
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  
+  def new
+    redirect_to '/auth/github'
+  end
+  
+  # Handles the Omniauth callback after a successful Github sign in. We first
+  # find or create the user by the username. If the user is not found, then we
+  # redirect to sign page with the error message as a flash. If the user was
+  # found, then we sign in and redirect.
+  def create
+    attributes = { :github_data => (data = request.env["omniauth.auth"][:extra][:raw_info]),
+                   :github_access_token => request.env["omniauth.auth"][:credentials][:token] }
+    user = User.find_or_create_by_username(data[:login], attributes)
+    
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => 'Successfully signed in via Github!'
+  end
+  
+  def destroy
+    reset_session
+    redirect_to root_url, :notice => 'You have been signed out. Come back soon now!'
+  end
+
+  def failure
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+  end
+  
+end
