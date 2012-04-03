16616a45d73125dc4aca0f15ccefed4a47d23ced
Joel Moss <joel@developwithstyle.com>
2012-04-03 11:04:29 +0100

    Lets try no sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { :namespace => "gitchen_#{Rails.env[0...3]}", :size => 25 }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { :namespace => "gitchen_#{Rails.env[0...3]}", :size => 1 }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { :namespace => "gitchen_#{Rails.env[0...3]}", :size => 25 }
+# end
+# 
+# Sidekiq.configure_client do |config|
+#   config.redis = { :namespace => "gitchen_#{Rails.env[0...3]}", :size => 1 }
+# end
