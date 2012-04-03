7ae6a2615b5775e84733f482b89ad0ddc3a14996
Joel Moss <joel@developwithstyle.com>
2012-04-03 10:00:53 +0100

    Moved bonsai config into Tire configure block

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,8 +1,8 @@
-if Rails.env.production?
-  Tire.configuration.url = "http://index.bonsai.io"
-  BONSAI_INDEX_NAME = URI.parse(ENV['BONSAI_INDEX_URL']).path[1..-1]
-end
-
 Tire.configure do
   logger Rails.root + "log/tire_#{Rails.env}.log"
+  
+  if Rails.env.production?
+    url "http://index.bonsai.io"
+    BONSAI_INDEX_NAME = URI.parse(ENV['BONSAI_INDEX_URL']).path[1..-1]
+  end
 end
