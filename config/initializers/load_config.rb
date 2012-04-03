55c5173f5b7a2bde364c812a3aef0f89b74c8215
Joel Moss <joel@developwithstyle.com>
2012-04-03 10:32:34 +0100

    Setting Gitchen config from ENV in production (heroku)

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,8 @@
-Gitchen::Config.configure :from_file => Rails.root.join('config', 'gitchen.yml')
+if Rails.env.production?
+  Gitchen::Config.configure do
+    github_key ENV['GITHUB_KEY']
+    github_secret ENV['GITHUB_SECRET']
+  end
+else
+  Gitchen::Config.configure :from_file => Rails.root.join('config', 'gitchen.yml')
+end
