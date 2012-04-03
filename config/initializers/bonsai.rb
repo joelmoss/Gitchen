d5374595ecdf146785fc21eb18c76b912570d236
Joel Moss <joel@developwithstyle.com>
2012-04-03 11:10:59 +0100

    Added comments about using on Heroku

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,8 @@
 Tire.configure do
   logger Rails.root + "log/tire_#{Rails.env}.log"
   
-  if Rails.env.production?
+  # Only needed if running on Heroku using the Bonsai addon.
+  if Rails.env.production? && ENV['BONSAI_INDEX_URL']
     url "http://index.bonsai.io"
   end
 end
